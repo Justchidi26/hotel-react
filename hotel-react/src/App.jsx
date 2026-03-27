c6ddfacf7483c04aed8f5fd301f875e25512087b
Justchidi26 <chidiaguwa26@gmail.com>
2026-03-28 00:11:31 +0100

    Add Hero section

--- hotel-react/src/App.jsx
+++ hotel-react/src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Navbar";
+import Hero from "./Hero";
 
 function App() {
 
   return (
     <>
-     <div className="flex flex-col">
+     <div className="flex flex-col items-center">
       <Navbar />
+      <Hero />
      </div>
     </>
   )
--- hotel-react/src/Navbar.jsx
+++ hotel-react/src/Navbar.jsx
@@ -5,22 +5,22 @@ import nigeriaflag from "./assets/nigeria-flag.svg";
 
 const Navbar = () => {
   return (
-    <div className="bg-sky-400 flex justify-between h-16">
+    <div className="bg-[#00aeff] flex justify-between h-15 w-screen">
       <div className="flex items-center px-4">
-        <div className=" px-4">
-          <img src={logo} alt="logo" className="h-8 w-20" />
+        <div className=" px-10">
+          <img src={logo} alt="logo" className="h-7 w-23" />
         </div>
       </div>
       <div className="flex h-full">
         <div className="border-r border-white flex items-center px-4">
-          <img src={whatsappicon} alt="whatsapp" className="h-10 w-10" />
+          <img src={whatsappicon} alt="whatsapp" className="h-8 w-8" />
         </div>
         <div className="border-r border-white flex items-center px-4">
-          <div className="flex items-center bg-blue-900 rounded-full pr-6 gap-6 h-11">
+          <div className="flex items-center bg-[#084482] rounded-full pr-6 gap-5 h-10 w-62">
             <img
               src={navbarContactMin}
               alt="contact"
-              className="h-12 w-12 rounded-full object-cover"
+              className="h-11 w-11 rounded-full object-cover"
             />
             <div className="flex flex-col items-center gap-[1px]">
               <div className="text-[10px] text-sky-500 mt-1">
@@ -37,7 +37,7 @@ const Navbar = () => {
           </div>
         </div>
         <div className="border-r border-white flex items-center px-4 text-white gap-2">
-          <img src={nigeriaflag} alt="Nigerian flag" className="h-10 w-10" />
+          <img src={nigeriaflag} alt="Nigerian flag" className="h-8 w-8" />
           <i class="text-white fa-solid fa-naira-sign"></i>
           <button>
             <i className="ml-2 mt-1 fa-solid fa-caret-down"></i>
