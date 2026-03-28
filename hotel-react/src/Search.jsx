ef741300003fb6afaed3fea043a84f06c167c6bf
Justchidi26 <chidiaguwa26@gmail.com>
2026-03-28 15:57:22 +0100

    Fix search form

--- hotel-react/src/Search.jsx
+++ hotel-react/src/Search.jsx
@@ -1,8 +1,8 @@
 const Search = () => {
   return (
-    <section className="bg-white shadow-xl/20 w-280 h-30 flex items-center justify-center">
-      <div className="bg-white pr-[30px] pl-[30px] flex justify-center items-center gap-2">
-        <div className="flex items-center border border-stone-400 w-120 p-2 rounded-sm">
+    <section className="w-full bg-white shadow-xl/20 h-30 flex items-center justify-center">
+      <div className="bg-white  flex justify-center items-center gap-1 px-10">
+        <div className="flex items-center border border-stone-400 w-120 p-2 rounded-md">
           <i class="fa-solid fa-magnifying-glass mr-2 text-[#4f4f4f]"></i>
           <input
             type="text"
@@ -10,14 +10,14 @@ const Search = () => {
             className="w-full outline-none"
           />
         </div>
-        <div className="flex items-center border border-stone-400 w-80 p-2 rounded-sm">
+        <div className="flex items-center border border-stone-400 w-70 p-2 rounded-md">
           <i class="fa-solid fa-calendar mr-2 text-[#4f4f4f]"></i>
           <input
             type="date"
             className="outline-none"
           />
         </div>
-        <button className="bg-[#00aeff] text-white cursor-pointer w-50 p-2 rounded-sm hover:bg-[#0099e6] transition duration-700 ease-in-out">
+        <button className="bg-[#00aeff] text-white cursor-pointer w-40 p-2 rounded-md hover:bg-[#0099e6] transition duration-700 ease-in-out">
           Find Hotels
         </button>
       </div>
