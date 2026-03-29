beb7ddac54c8e70610d4e386b7f8ce5c9f1b6225
Justchidi26 <chidiaguwa26@gmail.com>
2026-03-29 23:31:14 +0100

    Fix hover on photo card

--- hotel-react/src/Cities.jsx
+++ hotel-react/src/Cities.jsx
@@ -18,106 +18,116 @@ const Cities = () => {
       </div>
       <div className="flex flex-col gap-8">
         <div className="flex gap-8">
-          <div className="relative h-60 w-80 group cursor-pointer flex justify-center items-center">
+          <div className="relative h-60 w-80 group cursor-pointer flex justify-center items-center overflow-hidden rounded-sm">
             <div
               className="h-full w-full bg-cover bg-center flex items-center justify-center"
               style={{ backgroundImage: `url(${city1})` }}
             >
-              <div className="absolute inset-0 bg-black/40 opacity-100"></div>
-              <div className="relative z-10 flex flex-col items-center justify-center gap-1 text-center px-4">
+              <div className="absolute inset-0 bg-black/40 transition duration-300"></div>
+              <div className="absolute bottom-0 left-0 w-full group-hover:h-20 bg-black/40 group-hover:bg-black/60 transition"></div>
+              <div className="relative z-10 flex flex-col items-center justify-center gap-1 text-center px-4 mt-15">
                 <div className="text-white text-xl">3,642</div>
-                <div className="text-white text-xl font-medium">
+                <div className="text-white text-xl font-medium mb-10">
                   Lagos Hotels
                 </div>
+                <button className=" px-4 py-2 border border-white text-white rounded-md opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition duration-300">
+                  Book Hotels in Lagos
+                </button>
               </div>
             </div>
-            <div className="absolute inset-0 bg-black/40 opacity-50"></div>
           </div>
-          <div className="">
-            <div className="relative h-60 w-80 group cursor-pointer">
-              <div
-                className="h-full w-full bg-cover bg-center flex items-center justify-center"
-                style={{ backgroundImage: `url(${city2})` }}
-              >
-                <div className="absolute inset-0 bg-[rgba(51,51,51,0.5)]"></div>
-                <div className="relative z-10 flex flex-col items-center justify-center gap-1 text-center px-4">
-                  <div className="text-white text-xl">1,290</div>
-                  <div className="text-white text-xl font-medium">
-                    Abuja Hotels
-                  </div>
+          <div className="relative h-60 w-80 group cursor-pointer flex justify-center items-center overflow-hidden rounded-sm">
+            <div
+              className="h-full w-full bg-cover bg-center flex items-center justify-center"
+              style={{ backgroundImage: `url(${city2})` }}
+            >
+              <div className="absolute inset-0 bg-black/40 transition duration-300"></div>
+              <div className="absolute bottom-0 left-0 w-full group-hover:h-20 bg-black/40 group-hover:bg-black/60 transition"></div>
+              <div className="relative z-10 flex flex-col items-center justify-center gap-1 text-center px-4 mt-15">
+                <div className="text-white text-xl">1,290</div>
+                <div className="text-white text-xl font-medium mb-10">
+                  Abuja Hotels
                 </div>
+                <button className=" px-4 py-2 border border-white text-white rounded-md opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition duration-300">
+                  Book Hotels in Abuja
+                </button>
               </div>
-              <div className="absolute inset-0 bg-black/40 opacity-50"></div>
             </div>
           </div>
-          <div className="">
-            <div className="relative h-60 w-80 group cursor-pointer">
-              <div
-                className="h-full w-full bg-cover bg-center flex items-center justify-center"
-                style={{ backgroundImage: `url(${city3})` }}
-              >
-                <div className="absolute inset-0 bg-[rgba(51,51,51,0.5)]"></div>
-                <div className="relative z-10 flex flex-col items-center justify-center gap-1 text-center px-4">
-                  <div className="text-white text-xl">289</div>
-                  <div className="text-white text-xl font-medium">
-                    Calabar Hotels
-                  </div>
+          <div className="relative h-60 w-80 group cursor-pointer flex justify-center items-center overflow-hidden rounded-sm">
+            <div
+              className="h-full w-full bg-cover bg-center flex items-center justify-center"
+              style={{ backgroundImage: `url(${city3})` }}
+            >
+              <div className="absolute inset-0 bg-black/40 transition duration-300"></div>
+              <div className="absolute bottom-0 left-0 w-full group-hover:h-20 bg-black/40 group-hover:bg-black/60 transition"></div>
+              <div className="relative z-10 flex flex-col items-center justify-center gap-1 text-center px-4 mt-15">
+                <div className="text-white text-xl">289</div>
+                <div className="text-white text-xl font-medium mb-10">
+                  Calabar Hotels
                 </div>
+                <button className=" px-4 py-2 border border-white text-white rounded-md opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition duration-300">
+                  Book Hotels in Calabar
+                </button>
               </div>
-              <div className="absolute inset-0 bg-black/40 opacity-50"></div>
             </div>
           </div>
         </div>
         <div className="flex gap-8">
-          <div className="">
-            <div className="relative h-60 w-80 group cursor-pointer">
-              <div
-                className="h-full w-full bg-cover bg-center flex items-center justify-center"
-                style={{ backgroundImage: `url(${city4})` }}
-              >
-                <div className="absolute inset-0 bg-[rgba(51,51,51,0.5)]"></div>
-                <div className="relative z-10 flex flex-col items-center justify-center gap-1 text-center px-4">
-                  <div className="text-white text-xl">449</div>
-                  <div className="text-white text-xl font-medium">
-                    Port Harcourt Hotels
-                  </div>
+          <div className="relative h-60 w-80 group cursor-pointer flex justify-center items-center overflow-hidden rounded-sm">
+            <div
+              className="h-full w-full bg-cover bg-center flex items-center justify-center"
+              style={{ backgroundImage: `url(${city4})` }}
+            >
+              <div className="absolute inset-0 bg-black/40 transition duration-300"></div>
+              <div className="absolute bottom-0 left-0 w-full group-hover:h-20 bg-black/40 group-hover:bg-black/60 transition"></div>
+              <div className="relative z-10 flex flex-col items-center justify-center gap-1 text-center px-4 mt-15">
+                <div className="text-white text-xl">449</div>
+                <div className="text-white text-xl font-medium mb-10">
+                  Port Harcourt Hotels
                 </div>
+                <button className=" px-4 py-2 border border-white text-white rounded-md opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition duration-300">
+                  Book Hotels in Port Harcourt
+                </button>
               </div>
-              <div className="absolute inset-0 bg-black/40 opacity-50"></div>
             </div>
           </div>
           <div className="">
-            <div className="relative h-60 w-80 group cursor-pointer">
+            <div className="relative h-60 w-80 group cursor-pointer flex justify-center items-center overflow-hidden rounded-sm">
               <div
                 className="h-full w-full bg-cover bg-center flex items-center justify-center"
                 style={{ backgroundImage: `url(${city5})` }}
               >
-                <div className="absolute inset-0 bg-[rgba(51,51,51,0.5)]"></div>
-                <div className="relative z-10 flex flex-col items-center justify-center gap-1 text-center px-4">
+                <div className="absolute inset-0 bg-black/40 transition duration-300"></div>
+                <div className="absolute bottom-0 left-0 w-full group-hover:h-20 bg-black/40 group-hover:bg-black/60 transition"></div>
+                <div className="relative z-10 flex flex-col items-center justify-center gap-1 text-center px-4 mt-15">
                   <div className="text-white text-xl">259</div>
-                  <div className="text-white text-xl font-medium">
+                  <div className="text-white text-xl font-medium mb-10">
                     Owerri Hotels
                   </div>
+                  <button className=" px-4 py-2 border border-white text-white rounded-md opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition duration-300">
+                    Book Hotels in Owerri
+                  </button>
                 </div>
               </div>
-              <div className="absolute inset-0 bg-black/40 opacity-50"></div>
             </div>
           </div>
-          <div className="">
-            <div className="relative h-60 w-80 group cursor-pointer">
-              <div
-                className="h-full w-full bg-cover bg-center flex items-center justify-center"
-                style={{ backgroundImage: `url(${city6})` }}
-              >
-                <div className="absolute inset-0 bg-[rgba(51,51,51,0.5)]"></div>
-                <div className="relative z-10 flex flex-col items-center justify-center gap-1 text-center px-4">
-                  <div className="text-white text-xl">223</div>
-                  <div className="text-white text-xl font-medium">
-                    Uyo Hotels
-                  </div>
+          <div className="relative h-60 w-80 group cursor-pointer flex justify-center items-center overflow-hidden rounded-sm">
+            <div
+              className="h-full w-full bg-cover bg-center flex items-center justify-center"
+              style={{ backgroundImage: `url(${city6})` }}
+            >
+              <div className="absolute inset-0 bg-black/40 transition duration-300"></div>
+              <div className="absolute bottom-0 left-0 w-full group-hover:h-20 bg-black/40 group-hover:bg-black/60 transition"></div>
+              <div className="relative z-10 flex flex-col items-center justify-center gap-1 text-center px-4 mt-15">
+                <div className="text-white text-xl">223</div>
+                <div className="text-white text-xl font-medium mb-10">
+                  Uyo Hotels
                 </div>
+                <button className=" px-4 py-2 border border-white text-white rounded-md opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition duration-300">
+                  Book Hotels in Uyo
+                </button>
               </div>
-              <div className="absolute inset-0 bg-black/40 opacity-50"></div>
             </div>
           </div>
         </div>
