526cdfc1788689faae14c252cd08377b98f8d701
Justchidi26 <chidiaguwa26@gmail.com>
2026-03-30 21:39:36 +0100

    Edit the readme file

--- hotel-react/src/Footer.jsx
+++ hotel-react/src/Footer.jsx
@@ -1,69 +1,43 @@
 import nanta from "./assets/nanta.svg";
 import ntdc from "./assets/ntdc.svg";
+
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-3 mt-8 w-screen">
-      <div className="border-t border-stone-400 w-250"></div>
-      <div className="flex justify gap-40 text-stone-500">
-        <div className="flex gap-2">
+    <div className="flex flex-col items-center justify-center gap-6 mt-8 w-full px-4">
+      <div className="border-t border-stone-400 w-full max-w-6xl"></div>
+      <div className="flex md:flex-row md:justify-between items-center w-full max-w-6xl gap-6 text-stone-500">
+        <div className="flex md:flex-row items-center gap-4 text-center md:text-left">
+          
           <div className="text-sm">
             Copyright © 2026 Hotel Booking Limited All Rights Reserved
           </div>
-          <div className="flex gap-2">
-            <a
-              href="https://www.twitter.com/hotelsng"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+          <div className="flex gap-3 text-lg">
+            <a href="https://www.twitter.com/hotelsng" target="_blank" rel="noopener noreferrer">
               <i className="fa-brands fa-twitter cursor-pointer"></i>
             </a>
-            <a
-              href="https://www.facebook.com/hotelsng"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-facebook-f"></i>
+            <a href="https://www.facebook.com/hotelsng" target="_blank" rel="noopener noreferrer">
+              <i className="fa-brands fa-facebook-f cursor-pointer"></i>
             </a>
-            <a
-              href="https://instagram.com/hotelsng"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-instagram"></i>
+            <a href="https://instagram.com/hotelsng" target="_blank" rel="noopener noreferrer">
+              <i className="fa-brands fa-instagram cursor-pointer"></i>
             </a>
-            <a
-              href="https://www.google.com/+hotelsng"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-google"></i>
+            <a href="https://www.google.com/+hotelsng" target="_blank" rel="noopener noreferrer">
+              <i className="fa-brands fa-google cursor-pointer"></i>
             </a>
-            <a
-              href="https://pinterest.com/hotelsng"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-pinterest"></i>
+            <a href="https://pinterest.com/hotelsng" target="_blank" rel="noopener noreferrer">
+              <i className="fa-brands fa-pinterest cursor-pointer"></i>
             </a>
-            <a
-              href="https://www.youtube.com/user/hotelsng"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-youtube"></i>
+            <a href="https://www.youtube.com/user/hotelsng" target="_blank" rel="noopener noreferrer">
+              <i className="fa-brands fa-youtube cursor-pointer"></i>
             </a>
           </div>
         </div>
+        <div className="flex items-center gap-4">
+          <div className="text-sm">Certified by:</div>
 
-        <div className="flex gap-5">
-          <div className="text-sm mt-2">Certified by: </div>
-          <img src={nanta} alt="nanta" className="h-10 w-10" />
-          <div
-            className="h-10 w-10 bg-cover bg-center rounded-md"
-            style={{ backgroundImage: `url(${ntdc})` }}
-          ></div>
+          <img src={nanta} alt="nanta" className="h-10 w-10 object-contain" />
 
-          
+          <img src={ntdc} alt="ntdc" className="h-10 w-10 object-contain" />
         </div>
       </div>
     </div>
